5eb83945ffc37211aff81aeca92ef325b52fbfe5
Sabuj Golui <83773953+SabujGolui@users.noreply.github.com>
2023-04-15 21:16:18 +0530

    added footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "./Components/NavBar";
 import Timer from "./Pages/Timer";
 import Stopwatch from "./Pages/StopWatch";
+import FooterBar from "./Components/FooterBar";
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Timer/>}/>
         <Route path="/sw" element={<Stopwatch/>}/>
       </Routes>
+      <FooterBar />
     </Router>
   );
 }
---
+++ src/Components/FooterBar.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { AiFillGithub } from 'react-icons/ai';
+
+function FooterBar() {
+  return (
+    <footer className="footer fixed-bottom bg-light text-center text-lg-start">
+      <div
+        className="text-center p-4"
+        style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}
+      >
+        Made by Sabuj Golui. Get connected with me on GitHub:&nbsp;
+        <a className="text-dark" href="https://github.com/sabujgolui" rel="noopener noreferrer" target="_blank">
+          <AiFillGithub/>
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default FooterBar;
